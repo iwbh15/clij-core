db2926457be201a79e7dc9df2752ab66f2bee72a
haesleinhuepf <rhaase@mpi-cbg.de>
2019-07-26 20:49:09 +0200

    test for separable kernel bug-fix

---
+++ src/test/java/net/haesleinhuepf/clij/test/BlurUnsignedByteImageTest.java
@@ -0,0 +1,31 @@
+package net.haesleinhuepf.clij.test;
+
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
+import ij.gui.WaitForUserDialog;
+import net.haesleinhuepf.clij.CLIJ;
+import net.haesleinhuepf.clij.clearcl.ClearCLBuffer;
+import net.haesleinhuepf.clij.clearcl.ClearCLImage;
+import org.junit.Test;
+
+public class BlurUnsignedByteImageTest {
+    @Test
+    public void testBlur(){
+        ImagePlus imp = IJ.openImage("src/main/resources/blobs.tif");
+
+        CLIJ clij = CLIJ.getInstance();
+        ClearCLImage input = clij.convert(imp, ClearCLImage.class);
+        ClearCLBuffer output1 = clij.createCLBuffer(input.getDimensions(), input.getNativeType());
+        ClearCLBuffer output2 = clij.createCLBuffer(input.getDimensions(), input.getNativeType());
+
+        clij.op().blur(input, output1, 10f, 10f);
+        clij.op().blur(input, output2, 10f, 10f, 0f);
+
+        new ImageJ();
+        clij.show(output1, "out1");
+        clij.show(output2, "out2");
+
+        new WaitForUserDialog("ddd").show();
+    }
+}
